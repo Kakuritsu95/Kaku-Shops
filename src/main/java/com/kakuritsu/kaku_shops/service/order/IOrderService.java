a428401cf363800064900a007cb45cd413fb4d59
kakuritsu95 <ThodChristof@gmail.com>
2024-10-11 12:20:40 +0300

    Create calculateTotalAmount private method for OrderService

--- src/main/java/com/kakuritsu/kaku_shops/service/order/IOrderService.java
+++ src/main/java/com/kakuritsu/kaku_shops/service/order/IOrderService.java
@@ -1,9 +1,14 @@
 package com.kakuritsu.kaku_shops.service.order;
 
 import com.kakuritsu.kaku_shops.model.Order;
+import com.kakuritsu.kaku_shops.model.OrderItem;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 public interface IOrderService {
     Order placeOrder(Long userId);
+
     Order getOrder(Long orderId);
 }
 
--- src/main/java/com/kakuritsu/kaku_shops/service/order/OrderService.java
+++ src/main/java/com/kakuritsu/kaku_shops/service/order/OrderService.java
@@ -2,9 +2,13 @@ package com.kakuritsu.kaku_shops.service.order;
 
 import com.kakuritsu.kaku_shops.exceptions.ResourceNotFoundException;
 import com.kakuritsu.kaku_shops.model.Order;
+import com.kakuritsu.kaku_shops.model.OrderItem;
 import com.kakuritsu.kaku_shops.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -14,6 +18,14 @@ public class OrderService implements IOrderService {
     public Order placeOrder(Long userId) {
         return null;
     }
+
+
+    private BigDecimal calculateTotalAmount(List<OrderItem> orderItems) {
+       return orderItems.stream().reduce(BigDecimal.ZERO
+               ,(acc,curr)-> acc.add(curr.getPrice().multiply(new BigDecimal(curr.getQuantity())))
+               ,BigDecimal::add);
+    }
+
 
     @Override
     public Order getOrder(Long orderId) {
